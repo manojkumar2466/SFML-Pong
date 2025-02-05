0d669fe7e72d4d1029d3d8ed88fed8491fe7fe27
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2025-02-04 20:54:24 -0500

    ball texture added

--- Header/GamePlay/Ball.h
+++ Header/GamePlay/Ball.h
@@ -6,11 +6,21 @@ namespace GamePlay {
 
 	class Ball {
 	private:
-		CircleShape ball_Sprite;
+		CircleShape ball_circleShape_sprite;
 		const float radius = 10.f;
 		const float xPos = 230.f;
 		const float yPos = 100.f;
+
+		Texture ball_texture;
+		String ball_texture_path = "Assets/Textures/Ball.png";
+
+		Sprite ball_sprite;
+
+		float ball_sprite_x_scale = 0.2f, ball_sprite_y_scale= 0.2f;
 		
+
+		void LoadTexture();
+		void InitializeVariables();
 	public:
 		Ball();
 		void Update();
--- Source/GamePlay/Ball.cpp
+++ Source/GamePlay/Ball.cpp
@@ -1,11 +1,26 @@
 #include "D:\unity\repos\SFML-Pong\Header\GamePlay\Ball.h"
+#include <iostream>
 
 namespace GamePlay {
-
+	void Ball::LoadTexture()
+	{
+		if (!ball_texture.loadFromFile(ball_texture_path))
+		{
+			throw std::runtime_error("unable to load ball texture");
+		}
+	}
+	void Ball::InitializeVariables()
+	{
+		ball_sprite.setTexture(ball_texture);
+		ball_sprite.setScale(ball_sprite_x_scale, ball_sprite_y_scale);
+		ball_sprite.setPosition(xPos, yPos);
+	}
 	Ball::Ball()
 	{
-		ball_Sprite.setRadius(radius);
-		ball_Sprite.setPosition(xPos, yPos);
+		/*ball_circleShape_sprite.setRadius(radius);
+		ball_circleShape_sprite.setPosition(xPos, yPos);*/
+		LoadTexture();
+		InitializeVariables();
 
 	}
 	void Ball::Update()
@@ -13,6 +28,6 @@ namespace GamePlay {
 	}
 	void Ball::Render(RenderWindow* gameWindow)
 	{
-		gameWindow->draw(ball_Sprite);
+		gameWindow->draw(ball_sprite);
 	}
 }
