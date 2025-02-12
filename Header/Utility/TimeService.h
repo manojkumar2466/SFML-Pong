024e5ffcd2935d0219698348ada02c88682ce3a4
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2025-02-12 18:05:43 -0500

    delta time implemented

---
+++ Header/Utility/TimeService.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <chrono>
+
+namespace Utility
+{
+	class TimeService
+	{
+	private:
+
+		std::chrono::steady_clock::time_point previous_time;
+		float delta_time;
+
+		void UpdateDeltaTime();
+		float CalculateDeltaTime();
+		void UpdatePreviousTime();
+
+	public:
+		void Initialie();
+		void Update();
+		float GetDeltaTime();
+	};
+}
